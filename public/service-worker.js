93b0a5bc6008388b9682535773c7cade590c0a38
Jenn Greiner <jenn.greiner1@gmail.com>
2021-05-15 14:51:20 -0600

    manifest and service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,24 +1,33 @@
-const FILES_TO_CACHE = ["/", "index.html", "/manifest.json", ""];
-const STATIC_CACHE = "static-cache-v1";
-const RUNTIME_CACHE = "runtime-cache";
+const PRECACHE = "precache-v1";
+const RUNTIME = "runtime";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/assets/css/styles.css",
+  "/dist/manifest.json",
+  "/dist/app.bundle.js",
+  "/dist/assets/icons/icon_192x192.png",
+  "/dist/assets/icons/icon_512x512.png",
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches
-      .open(STATIC_CACHE)
+      .open(PRECACHE)
       .then((cache) => cache.addAll(FILES_TO_CACHE))
-      .then(() => self.skipWaiting())
+      .then(self.skipWaiting())
   );
 });
 
 // The activate handler takes care of cleaning up old caches.
 self.addEventListener("activate", (event) => {
-  const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+  const currentCaches = [PRECACHE, RUNTIME];
   event.waitUntil(
     caches
       .keys()
       .then((cacheNames) => {
-        // return array of cache names that are old to delete
         return cacheNames.filter(
           (cacheName) => !currentCaches.includes(cacheName)
         );
@@ -35,9 +44,9 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  // non GET requests are not cached and requests to other origins are not cached
+  // non POST requests are not cached and requests to other origins are not cached
   if (
-    event.request.method !== "GET" ||
+    event.request.method !== "POST" ||
     !event.request.url.startsWith(self.location.origin)
   ) {
     event.respondWith(fetch(event.request));
@@ -45,13 +54,13 @@ self.addEventListener("fetch", (event) => {
   }
 
   // handle runtime GET requests for data from /api routes
-  if (event.request.url.includes("/api/images")) {
+  if (event.request.url.includes("/api/transaction")) {
     // make network request and fallback to cache if network request fails (offline)
     event.respondWith(
-      caches.open(RUNTIME_CACHE).then((cache) => {
+      caches.open(RUNTIME).then((cache) => {
         return fetch(event.request)
           .then((response) => {
-            cache.put(event.request, response.clone());
+            cache.put(event.request.url, response.clone());
             return response;
           })
           .catch(() => caches.match(event.request));
@@ -68,7 +77,7 @@ self.addEventListener("fetch", (event) => {
       }
 
       // request is not in cache. make network request and cache the response
-      return caches.open(RUNTIME_CACHE).then((cache) => {
+      return caches.open(RUNTIME).then((cache) => {
         return fetch(event.request).then((response) => {
           return cache.put(event.request, response.clone()).then(() => {
             return response;
@@ -78,3 +87,5 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
+console.log("Hi from your service-worker.js file!");
