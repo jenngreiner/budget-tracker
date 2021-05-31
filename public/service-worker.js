36ab514eb8dccaedc765efbf0331d541d41df449
Jenn Greiner <75277268+jenngreiner@users.noreply.github.com>
2021-05-31 12:20:12 -0600

    restart / service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,91 +1,63 @@
-const PRECACHE = "precache-v1";
-const RUNTIME = "runtime";
+var CACHE_NAME = "my-site-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
 
-const FILES_TO_CACHE = [
+var FILES_TO_CACHE = [
   "/",
-  "/index.html",
+  "/db.js",
+  "/index.js",
   "/manifest.json",
-  "/assets/css/styles.css",
-  "/dist/manifest.json",
-  "/dist/app.bundle.js",
-  "/dist/assets/icons/icon_192x192.png",
-  "/dist/assets/icons/icon_512x512.png",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", function (event) {
+  // Perform install steps
   event.waitUntil(
-    caches
-      .open(PRECACHE)
-      .then((cache) => cache.addAll(FILES_TO_CACHE))
-      .then(self.skipWaiting())
-  );
-});
-
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener("activate", (event) => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) => {
-        return cacheNames.filter(
-          (cacheName) => !currentCaches.includes(cacheName)
-        );
-      })
-      .then((cachesToDelete) => {
-        return Promise.all(
-          cachesToDelete.map((cacheToDelete) => {
-            return caches.delete(cacheToDelete);
-          })
-        );
-      })
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  // non POST requests are not cached and requests to other origins are not cached
-  if (
-    event.request.method !== "POST" ||
-    !event.request.url.startsWith(self.location.origin)
-  ) {
-    event.respondWith(fetch(event.request));
-    return;
-  }
-
-  // handle runtime GET requests for data from /api routes
-  if (event.request.url.includes("/api/transaction")) {
-    // make network request and fallback to cache if network request fails (offline)
-    event.respondWith(
-      caches.open(RUNTIME).then((cache) => {
-        return fetch(event.request)
-          .then((response) => {
-            cache.put(event.request.url, response.clone());
-            return response;
-          })
-          .catch(() => caches.match(event.request));
-      })
-    );
-    return;
-  }
-
-  // use cache first for all other requests for performance
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-
-      // request is not in cache. make network request and cache the response
-      return caches.open(RUNTIME).then((cache) => {
-        return fetch(event.request).then((response) => {
-          return cache.put(event.request, response.clone()).then(() => {
-            return response;
-          });
-        });
-      });
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
 
-console.log("Hi from your service-worker.js file!");
+self.addEventListener("fetch", function (event) {
+  // cache all get requests to /api routes
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(event.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(event.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(function () {
+      return caches.match(event.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
